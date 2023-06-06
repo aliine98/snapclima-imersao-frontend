8ce9611413b54e70b8804e00ab24eb75f9637fcb
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2023-06-06 02:17:47 -0300

    ✨ feat: get current location

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
+import fetchCurrentWeather from "./fetchCurrentWeather.js";
+
 const darkLightButton = document.querySelector(".menu__button");
 
 darkLightButton.addEventListener("click", () => {
 	const body = document.querySelector("body");
 	body.classList.toggle("dark-mode");
 });
+
+//get location if supported by the browser
+if ("geolocation" in navigator) {
+	navigator.geolocation.getCurrentPosition((pos) => {
+		const { latitude, longitude } = pos.coords;
+		fetchCurrentWeather(latitude, longitude);
+	});
+}
