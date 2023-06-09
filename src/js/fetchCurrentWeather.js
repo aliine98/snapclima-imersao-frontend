514e9582b4e5f279b360da7dc592d63d4499c639
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2023-06-09 01:16:40 -0300

    ✨ feat: add renderCityWeatherInfos function
    To add the weather information fetched from the api

--- src/js/fetchCurrentWeather.js
+++ src/js/fetchCurrentWeather.js
@@ -1,10 +1,10 @@
 import { API_KEY } from "./API_KEY.js";
-import { renderWeather } from "./renderWeather.js";
+import { renderCityWeatherInfos } from "./renderCityWeatherInfos.js";
 
 export default function fetchCurrentWeather(latitude, longitude) {
 	fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=pt_br`)
 		.then((response) => response.json())
 		.then((data) => {
-			renderWeather(data);
+			renderCityWeatherInfos(data);
 		});
 }
