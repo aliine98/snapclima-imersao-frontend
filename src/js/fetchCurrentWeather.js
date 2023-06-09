e951191ce96b45dd7007f63c3486c08d0c179061
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2023-06-09 14:24:38 -0300

    ♻️ refactor: change to async/await

--- src/js/fetchCurrentWeather.js
+++ src/js/fetchCurrentWeather.js
@@ -1,10 +1,8 @@
 import { API_KEY } from "./API_KEY.js";
 import { renderCityWeatherInfos } from "./renderCityWeatherInfos.js";
 
-export default function fetchCurrentWeather(latitude, longitude) {
-	fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=pt_br`)
-		.then((response) => response.json())
-		.then((data) => {
-			renderCityWeatherInfos(data);
-		});
+export default async function fetchCurrentWeather(latitude, longitude) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=pt_br`);
+    const data = await response.json();
+    renderCityWeatherInfos(data);
 }
