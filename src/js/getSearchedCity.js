d7d8cf0c95dcf282845f7c2038bf664a9ff79c2a
Aline Bevilacqua <90913523+Aliine98@users.noreply.github.com>
2023-06-10 14:59:09 -0300

    🐛 fix: change to https

--- src/js/getSearchedCity.js
+++ src/js/getSearchedCity.js
@@ -13,7 +13,7 @@ export async function getSearchedCity() {
     const city = document.querySelector(".header__input").value;
 
     //Get lat and lon of the city from Geocoding API
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`);
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`);
     const data = await response.json();
     const { lat, lon } = data[0];
 
