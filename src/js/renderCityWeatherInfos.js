2643d847a9a753b342b982d0f63dc7c17093d970
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2023-07-06 16:39:00 -0300

    🔒️ refactor: change innerHTML to textContent

--- src/js/renderCityWeatherInfos.js
+++ src/js/renderCityWeatherInfos.js
@@ -19,22 +19,22 @@ export function renderCityWeatherInfos({ weather, main, sys, wind, dt, name }) {
     const { sunrise, sunset } = sys;
 
     //Header info
-    document.querySelector(".header__current-date").innerHTML = Intl.DateTimeFormat("pt-br", {
+    document.querySelector(".header__current-date").textContent = Intl.DateTimeFormat("pt-br", {
         weekday: "short",
         month: "long",
         day: "2-digit",
     }).format(new Date(dt * 1000));
-    document.querySelector(".header__city-name").innerHTML = name;
+    document.querySelector(".header__city-name").textContent = name;
 
     //Current temperature info
     document.querySelector(".current-temperature__weather-icon").src = `./src/assets/${icon}.svg`;
-    document.querySelector(".current-temperature__description").innerHTML = description;
-    document.querySelector(".current-temperature__value").innerHTML = `${Math.round(temp)}°C`;
+    document.querySelector(".current-temperature__description").textContent = description;
+    document.querySelector(".current-temperature__value").textContent = `${Math.round(temp)}°C`;
 
     //Bottom container info
-    document.getElementById("feels-like").innerHTML = `${Math.round(feels_like)}°C`;
-    document.getElementById("wind").innerHTML = `${Math.round(speed * 3.6)} km/h`;
-    document.getElementById("humidity").innerHTML = `${Math.round(humidity)}%`;
-    document.getElementById("sunrise").innerHTML = `${Intl.DateTimeFormat("pt-br", { timeStyle: "short" }).format(new Date(sunrise * 1000))}`;
-    document.getElementById("sunset").innerHTML = `${Intl.DateTimeFormat("pt-br", { timeStyle: "short" }).format(new Date(sunset * 1000))}`;
+    document.getElementById("feels-like").textContent = `${Math.round(feels_like)}°C`;
+    document.getElementById("wind").textContent = `${Math.round(speed * 3.6)} km/h`;
+    document.getElementById("humidity").textContent = `${Math.round(humidity)}%`;
+    document.getElementById("sunrise").textContent = `${Intl.DateTimeFormat("pt-br", { timeStyle: "short" }).format(new Date(sunrise * 1000))}`;
+    document.getElementById("sunset").textContent = `${Intl.DateTimeFormat("pt-br", { timeStyle: "short" }).format(new Date(sunset * 1000))}`;
 }
